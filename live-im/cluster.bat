03aa94e37eb2309f09746af2c7fe51f9efa55f80
Alan Zhang <64122416+heartape@users.noreply.github.com>
2023-10-31 23:17:50 +0800

    polish im

--- live-im/cluster.bat
+++ live-im/cluster.bat
@@ -4,12 +4,12 @@
 
 ssh root@192.168.31.101 "mkdir -p /opt/im;systemctl start sshd"
 scp -r target/live-im.jar root@192.168.31.101:/opt/im/live-im.jar
-ssh root@192.168.31.101 "java -jar /opt/im/live-im.jar"
+start powershell -Command ssh root@192.168.31.101 "source /etc/profile;java -jar /opt/im/live-im.jar"
 
 ssh root@192.168.31.102 "mkdir -p /opt/im;systemctl start sshd"
 scp -r target/live-im.jar root@192.168.31.102:/opt/im/live-im.jar
-ssh root@192.168.31.102 "java -jar /opt/im/live-im.jar"
+start powershell -Command ssh root@192.168.31.102 "source /etc/profile;java -jar /opt/im/live-im.jar"
 
 ssh root@192.168.31.103 "mkdir -p /opt/im;systemctl start sshd"
 scp -r target/live-im.jar root@192.168.31.103:/opt/im/live-im.jar
-ssh root@192.168.31.103 "java -jar /opt/im/live-im.jar"
+start powershell -Command ssh root@192.168.31.103 "source /etc/profile;java -jar /opt/im/live-im.jar"
